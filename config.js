d5472d6f243dcd325b98187e07a7b13d0f4a81d9
XiCave <manmaxy2@gmail.com>
2024-05-24 23:20:25 +0700

    SAITAMA

---
+++ config.js
@@ -0,0 +1,48 @@
+// Buat Lu Yang Jual Sc Ini Yang Jujur Jangan Samp Nipu Apalagi Lari Dari Tanggung Jawab dekk minim jual 10k - 20k
+
+//==============[ By Always Ziyo Tzy ]==========//
+
+const fs = require('fs')
+//=============[ ----- ]============//
+
+//==========================//
+
+// Sosial Media 
+global.gr = 'http://www.ziyoo.com/' // gausah di ubah :v
+global.wagc = "https://chat.whatsapp.com/Ey04dtjCOJc3rYm514R4zs" // yg ini silahkan ubah
+global.ig = 'https://www.instagram.com/rezaaaa_s13' // ubah aja
+global.email = 'daffzkece@gmail.com' //serah
+global.region = 'indonesia' // serah
+global.ownername = 'Cave?' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.owner = ['6283128526227'] // ubah aja pake nomor lu
+global.wanumber = ['6283128526227'] // ketika ketik .owner
+global.botname = 'exBOT' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+//==========================[ Masukin Apikey nya ]=======================//
+global.keyopenai = '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'
+//====================[ BY Hw Mods]=============================//
+global.packname = 'sticker' // ubah aja ini nama sticker
+global.author = 'Cave?' // ubah aja ini nama sticker
+global.prefa = ['','!','.',',','🐤','🗿']
+global.sessionName = 'Saitama' //Gausah Juga
+global.sp = '⭔' // Gausah Juga
+global.wlcm = []
+global.wlcmm = []
+global.anticall = false // true = auto blok kalo ad yg telfon
+//==============================7===================//
+// Batas User nya
+global.limitawal = {
+    premium: "Infinity",
+    free: 1000
+}
+// taaofc
+global.thum = fs.readFileSync("./basezio/image/thum.jpeg") 
+global.verifsukses = fs.readFileSync ("./basezio/image/sukses.jpeg")
+global.dash = fs.readFileSync("./basezio/image/dashboar.jpeg")
+//=========================[ BATAS COOEG ]========================//
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {7777
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
